98dae770a707f48682052cfb1fedba9ca83d847b
danielevergani <dvergani94@gmail.com>
2021-03-09 15:39:33 +0100

    utente può inserire x numeri, senza duplicati

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// genero x num casuali univoci
+
 var listaCasuali = [];
 
 for ( var i = 0; i<5; i++) {
@@ -16,3 +18,32 @@ for ( var i = 0; i<5; i++) {
 }
 
 console.log(listaCasuali);
+
+// utente deve inserire x numeri diversi tra di loro
+
+var sconfitta = false;
+var numSceltoLista = [];
+var j = 0;
+var numScelto = 0;
+
+while ( j <= 5 && sconfitta == false ){
+
+    numScelto = parseInt(prompt("inserisci un numero"));
+    // controllo non ci siano duplicati
+    if ( numSceltoLista.includes(numScelto) ){
+
+        do{
+            numScelto = parseInt(prompt("inserisci un numero"));
+        } while ( numSceltoLista.includes(numScelto) );
+        
+        numSceltoLista.push(numScelto)
+        console.log(numSceltoLista)
+    }
+    else {
+        numSceltoLista.push(numScelto)
+        console.log(numSceltoLista)
+    }
+
+    j++;
+    console.log(j);
+}
