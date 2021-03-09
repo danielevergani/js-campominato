49ab65bc466b18ab03857a50df9b4b566e88eb8e
danielevergani <dvergani94@gmail.com>
2021-03-09 16:02:34 +0100

    ok funzione x generare num casuali

--- js/script.js
+++ js/script.js
@@ -1,20 +1,13 @@
 // genero x num casuali univoci
 
 var listaCasuali = [];
+var difficolta = 10;
 
-for ( var i = 0; i<5; i++) {
+for ( var i = 0; i<difficolta; i++) {
     
-    var numero = Math.floor(Math.random() * 5) + 1;
-    if ( listaCasuali.includes(numero) ){
-        do{
-            numero = Math.floor(Math.random() * 5) + 1;
-        } while (listaCasuali.includes(numero));
-        listaCasuali.push(numero)
-    }
-    else {
-        listaCasuali.push(numero)
-    }
-    
+    var numero = Math.floor(Math.random() * 100) + 1;
+    verificaPresenza(listaCasuali, numero)
+
 }
 
 console.log(listaCasuali);
@@ -25,8 +18,10 @@ var sconfitta = false;
 var numSceltoLista = [];
 var j = 0;
 var numScelto = 0;
+var contatore = 0;
+var tentativi = parseInt(prompt("quanti tentativi vuoi avere?"));
 
-while ( j <= 5 && sconfitta == false ){
+while ( j < tentativi && sconfitta == false ){
 
     numScelto = parseInt(prompt("inserisci un numero"));
     // controllo non ci siano duplicati
@@ -44,6 +39,36 @@ while ( j <= 5 && sconfitta == false ){
         console.log(numSceltoLista)
     }
 
+    if ( listaCasuali.includes(numScelto)){
+        sconfitta = true
+        contatore = j + 1
+    }
+
     j++;
     console.log(j);
 }
+
+alert("BOOM!! hai perso al " + contatore + " tentativo");
+
+function verificaPresenza(array, num){
+    if ( array.includes(num) ){
+        do{
+            num = Math.floor(Math.random() * 100) + 1;
+        } while (array.includes(num));
+        array.push(num)
+    }
+    else {
+        array.push(num)
+    }
+}
+
+// SEZIONE PER CREAZIONE LISTA DI NUMERI CASUALI SENZA UTILIZZO DI FUNZIONE
+// if ( listaCasuali.includes(numero) ){
+    //     do{
+    //         numero = Math.floor(Math.random() * 5) + 1;
+    //     } while (listaCasuali.includes(numero));
+    //     listaCasuali.push(numero)
+    // }
+    // else {
+    //     listaCasuali.push(numero)
+    // }
