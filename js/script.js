3a15691eed57b4873b5cf62bc8ea4f0b343df74d
danielevergani <dvergani94@gmail.com>
2021-03-09 15:24:05 +0100

    prog genera num casuali diversi tra loro e pusha in un array

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-alert();
+var listaCasuali = [];
+
+for ( var i = 0; i<5; i++) {
+    
+    var numero = Math.floor(Math.random() * 5) + 1;
+    if ( listaCasuali.includes(numero) ){
+        do{
+            numero = Math.floor(Math.random() * 5) + 1;
+        } while (listaCasuali.includes(numero));
+        listaCasuali.push(numero)
+    }
+    else {
+        listaCasuali.push(numero)
+    }
+    
+}
+
+console.log(listaCasuali);
